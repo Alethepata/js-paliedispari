c2953a98d3d10c72530709afa4b32a52b03c8916
Alethepata <alessia.patane@gmail.com>
2023-09-15 19:04:43 +0200

    [Add] btn

--- js/script.js
+++ js/script.js
@@ -1,34 +1,45 @@
-// PAROLA PALINDROMA
 
+// PAROLA PALINDROMA
 
 const content = document.getElementById('content');
 
-const word = prompt('Inserire parola');
+const inputWord = document.getElementById('input-word');
+
+const btn1= document.getElementById('btn-1');
+
+btn1.addEventListener('click', function(){
+const word = inputWord.value;
+
 const reverse = word.split('').reverse().join('');
+
 let wordPalindrome = palindrome(word);
-
-
 
 function palindrome (word){
     if(word == reverse) return true
     return false
 }
-
 if(wordPalindrome){
     message = 'La parola ' + word + ' è polindoma'; 
 }else {
     message = 'La parola ' + word + ' non è polindoma'; 
 }
-
 content.innerHTML = message;
+
+});
+
 
 
 //PARI O DISPARI
 
 const secondContent = document.getElementById('second-content');
+const inputnum = document.getElementById('input-num');
+const inputEvenOdd = document.getElementById('input-even-odd');
+const btn2= document.getElementById('btn-2');
 
-const numPlayer =parseInt(prompt('Inserire un numero')); 
-const  evenOddPlayer=prompt('Pari o dispari');
+btn2.addEventListener('click', function(){
+
+const numPlayer =parseInt(inputnum.value); 
+const  evenOddPlayer=inputEvenOdd.value;
 const numComputer =randomizer(1,5);
 const sum = numPlayer + numComputer;
 const result = evenOdd(sum);
@@ -36,31 +47,17 @@ const messageresult = resultEvenOdd(result);
 let messageDue;
 
 
-
 function randomizer (min, max){
     return Math.floor( Math.random()*(max - min +1) + min);
 }
-
 function evenOdd (value){
     if(value % 2) return 'dispari'
     else if( !(value % 2)) return 'pari'
 }
-
-
-
-
 function resultEvenOdd (result){
     if(evenOddPlayer === result) return true
     return false 
 }
-
-
-function palindrome (word){
-    if(word == reverse) return true
-    return false
-}
-
-
 if(messageresult){
     messageDue = 'Hai vinto ' ; 
 }else {
@@ -69,3 +66,6 @@ if(messageresult){
 
 secondContent.innerHTML = messageDue + ' perchè tu hai scelto ' + numPlayer + ' ' + evenOddPlayer + 
 '. Il Computer ha scelto ' + numComputer + '. La somma è ' + sum;
+})
+
+
