e8f7fcb3890b13e3f4da028c37efaea68d9a9da0
Alethepata <alessia.patane@gmail.com>
2023-09-15 16:24:05 +0200

    [Add] if

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
-const parola = ('enne');
-const reverse = parola.split('').reverse().join('');
-let parolaPalindroma = palindroma(parola);
-console.log(parolaPalindroma);
+const word = prompt('Inserire parola');
+const reverse = word.split('').reverse().join('');
+let wordPalindrome = palindrome(word);
 
-function palindroma (parola){
-    if(parola == reverse) return true
+function palindrome (word){
+    if(word == reverse) return true
     return false
 }
+
+if(wordPalindrome){
+    message = 'si'; 
+}else {
+    message = 'no'; 
+}
+
+console.log(message);
